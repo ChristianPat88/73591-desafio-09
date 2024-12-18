0b653c71ed26d3387411365e23d8ae1aa430f09e
Christian Patiño <christiandp88@gmail.com>
2024-12-18 20:35:52 -0300

    Se escribio el codigo js para el ejercicio c en el archivo ejercicio-c.js

--- src/ejercicio-b.js
+++ src/ejercicio-b.js
@@ -5,14 +5,15 @@ console.warn('Ejercicio B')
 let gato = '🐈'
 let paso = '🐾'
 
-let cantidadDeGatosB = 5
+let cantidadDeGatos = 5
 let cantidadDePasos = 3
 
 let pasos = ''
-for (let i = 0; i < cantidadDeGatosB; i++) {
+for (let i = 0; i < cantidadDeGatos; i++) {
   for (let j = 0; j < cantidadDePasos; j++) {
     pasos+=paso
   }
+  
   console.log(`Gato #${i + 1}: ${gato} ${pasos}`)
   pasos = ''
 }
--- src/ejercicio-c.js
+++ src/ejercicio-c.js
@@ -2,3 +2,23 @@
 /* EJERCICIO C --------------------------------------------------- */
 console.warn('Ejercicio C')
 
+let gato = '🐈'
+let gatoNegro = '🐈‍⬛'
+let paso = '🐾'
+
+let cantidadDeGatos = 10
+let cantidadDePasos = 4
+
+let pasos = ''
+let gatos = ''
+for (let i = 0; i < cantidadDeGatos; i++) {
+  for (let j = 0; j < cantidadDePasos; j++) {
+    pasos+=paso
+  }
+  
+  gatos = ((i + 1) % 2) > 0 ? gato : (gato + gatoNegro)
+
+  console.log(`Gato #${i + 1}: ${gatos} ${pasos}`)
+  pasos = ''
+}
+
