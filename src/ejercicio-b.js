3247544b157b1a44a706986e285f1d4ae8239388
Christian Patiño <christiandp88@gmail.com>
2024-12-18 20:19:18 -0300

    Se separaron los codigos de cada ejercicio en 3 diferentes archivos js, a su vez se modifico el index.html para invocarlos correctamente

--- src/ejercicio-b.js
+++ src/ejercicio-b.js
@@ -1,18 +1,3 @@
-import './style.css'
-
-/* EJERCICIO A --------------------------------------------------- */
-console.warn('Ejercicio A')
-
-let gatos = ['😺', '😸', '😹']
-
-let cantidadDeGatos = 10
-
-let cont = 0
-for (let i = 0; i < cantidadDeGatos; i++) {
-  console.log(`Gato #${i + 1}: ${gatos[cont]}`)
-  cont === 2 ? cont = 0 : cont++
-}
-
 
 /* EJERCICIO B --------------------------------------------------- */
 console.warn('Ejercicio B')
